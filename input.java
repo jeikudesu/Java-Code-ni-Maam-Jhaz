b9c52692739f8782dc64c33fbf7c08178f0461f4
Jake Gomez <jake.ggvi@gmail.com>
2025-11-20 08:32:11 +0800

    Create input.java

---
+++ input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// Main class to run all activities
+public class OOPActivities {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 1. Input (1 only)
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
+
+        System.out.println();
